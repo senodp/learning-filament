6c980ee4923bb5f8ddf141f8e92db9ff47419ea6
Seno Dwi Prasetyo <senothoha77@gmail.com>
2023-12-07 08:05:45 +0700

    category - CRUD

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -13,6 +13,14 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+//use Closure;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Str;
+use Filament\Forms\Set;
+
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Card;
+
 class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
@@ -23,15 +31,26 @@ class CategoryResource extends Resource
     {
         return $form
             ->schema([
-                //
-            ]);
+            Card::make()->schema([
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255)
+                ->live()
+                ->afterStateUpdated(function (Set $set, $state) {
+                $set('slug', Str::slug($state));
+            }),  
+            TextInput::make('slug')->required()
+            ])
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id'),
+                TextColumn::make('name')->limit('50')->sortable(),
+                TextColumn::make('slug')->limit('50')
             ])
             ->filters([
                 //
