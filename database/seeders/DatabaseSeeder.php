92c4f9d662d1597f43ed37c106880210928c25ef
Seno Dwi Prasetyo <senothoha77@gmail.com>
2023-12-12 11:05:32 +0700

    Role and permission - 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@komunigrafik.com',
+        ]);
+
+        $user2 = User::factory()->create([
+            'name' => 'Editor',
+            'email' => 'editor@komunigrafik.com',
+        ]);
+
+        $role = Role::create(['name' => 'admin']);
+        $user1->assignRole($role);
+
+        $role = Role::create(['name' => 'editor']);
+        $user1->assignRole($role);
     }
 }
