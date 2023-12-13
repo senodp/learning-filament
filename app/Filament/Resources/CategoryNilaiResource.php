096ed13ad648f2b7af7f1d4413311aab6870d39d
Seno Dwi Prasetyo <senothoha77@gmail.com>
2023-12-13 14:40:03 +0700

    Simple resource - ver. 3

---
+++ app/Filament/Resources/CategoryNilaiResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryNilaiResource\Pages;
+use App\Filament\Resources\CategoryNilaiResource\RelationManagers;
+use App\Models\CategoryNilai;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+// komponen untuk form
+use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Str;
+use Filament\Forms\Set;
+// komponen untuk tabel
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Card;
+
+use Filament\Tables\Contracts\HasTable; //untuk number sort
+
+class CategoryNilaiResource extends Resource
+{
+    protected static ?string $model = CategoryNilai::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLabel = 'Category Nilai';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+            Card::make()->schema([
+            TextInput::make('name')
+                ->required()
+                ->maxLength(255)
+                ->live()
+                ->afterStateUpdated(function (Set $set, ?string $state) {
+                $set('slug', \Str::slug($state));
+            }),  
+            TextInput::make('slug')->required()
+            ])
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('No.')->state(
+                static function (HasTable $livewire, $rowLoop): string {
+                    return (string) (
+                        $rowLoop->iteration +
+                        ($livewire->getTableRecordsPerPage() * (
+                            $livewire->getTablePage() - 1
+                        ))
+                    );
+                }
+            ),
+                TextColumn::make('name')->limit('50')->sortable(),
+                TextColumn::make('slug')->limit('50')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageCategoryNilais::route('/'),
+        ];
+    }
+}
