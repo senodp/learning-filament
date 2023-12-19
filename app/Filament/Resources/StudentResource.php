467c0621ac1bfc5c05084dd8fbcea353ff10e56f
Seno Dwi Prasetyo <senothoha77@gmail.com>
2023-12-19 10:05:01 +0700

    BulkAction

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -13,6 +13,8 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+use Illuminate\Database\Eloquent\Collection;
+
 // komponen untuk form
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\TextArea;
@@ -26,6 +28,12 @@ use Filament\Tables\Contracts\HasTable; //untuk number sort
 
 use Filament\Forms\Components\FileUpload; //untuk upload
 use Filament\Tables\Columns\ImageColumn; //untuk tampilkan file
+
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+use Filament\Infolists\Components\TextEntry;
+
+use Filament\Tables\Actions\BulkAction;
 
 class StudentResource extends Resource
 {
@@ -82,17 +90,36 @@ class StudentResource extends Resource
                 TextColumn::make('religion'),
                 TextColumn::make('contact'),
                 //Textarea::make('address'),
-                ImageColumn::make('profile')
+                ImageColumn::make('profile'),
+                TextColumn::make('status')
+                    ->formatStateUsing(fn (string $state): string => ucwords("{$state}"))
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                //untuk menampilkan popup
+                Tables\Actions\ViewAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    bulkAction::make('Accept')
+                        ->icon('heroicon-m-check')
+                        ->requiresConfirmation()
+                        ->action(function (Collection $records){
+                            return $records->each->update(['status'=>'accept']);
+                        }),
+                    bulkAction::make('Off')
+                        ->icon('heroicon-m-x-circle')
+                        ->requiresConfirmation()
+                        ->action(function (Collection $records){
+                            return $records->each(function ($record){
+                                $id = $record->id;
+                                Student::where('id', $id)->update(['status'=>'off']);
+                            });
+                        }),
+                Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
@@ -110,6 +137,21 @@ class StudentResource extends Resource
             'index' => Pages\ListStudents::route('/'),
             'create' => Pages\CreateStudent::route('/create'),
             'edit' => Pages\EditStudent::route('/{record}/edit'),
+            //untuk menampilkan halaman baru
+            //'view' => Pages\ViewStudent::route('/{record}')
         ];
     }
+
+    // public static function infolist(Infolist $infolist): Infolist
+    // {
+    //     return $infolist
+    //         ->schema([
+    //             TextEntry::make('nis'),
+    //             TextEntry::make('name'),
+    //             TextEntry::make('gender'),
+    //             TextEntry::make('birthday'),
+    //             TextEntry::make('religion'),
+    //             TextEntry::make('contact')
+    //         ]);
+    // }
 }
