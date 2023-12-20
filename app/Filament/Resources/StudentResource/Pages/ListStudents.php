0840ec2c84478550bbde38c3b82ee9013483f718
Seno Dwi Prasetyo <senothoha77@gmail.com>
2023-12-20 09:48:32 +0700

    Tab filter

--- app/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\StudentResource\Pages;
 
+use Filament\Resources\Components\Tab;
+use Illuminate\Database\Eloquent\Builder;
+
 use App\Filament\Resources\StudentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
@@ -14,6 +17,21 @@ class ListStudents extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'accept' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'accept')),
+            'off' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'off')),
+            'male' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('gender', 'male')),
+            'female' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('gender', 'female')),
         ];
     }
 }
