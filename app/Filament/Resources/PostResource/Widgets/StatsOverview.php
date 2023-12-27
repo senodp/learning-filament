bc0f46460c79e8481b2a03d4236db6420139a8f9
Seno Dwi Prasetyo <senothoha77@gmail.com>
2023-12-27 09:35:46 +0700

    pivot table - cek again

--- app/Filament/Resources/PostResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/PostResource/Widgets/StatsOverview.php
@@ -6,7 +6,9 @@ use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 use App\Models\Post;
-
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\Subject;
 //namespace App\Filament\Widgets;
 
 class StatsOverview extends BaseWidget
@@ -17,8 +19,13 @@ class StatsOverview extends BaseWidget
             Stat::make('Post', Post::all()->count()),
             //->description('32k increase'),
             //->descriptionIcon('heroicon-m-arrow-trending-up'),
-            Stat::make('Publish', Post::where('status', TRUE)->count()),
-            Stat::make('Hidden', Post::where('status', FALSE)->count()),
+            Stat::make('Publish', Post::where('status', TRUE)->count())
+                ->description('Post Publish'),
+            Stat::make('Hidden', Post::where('status', FALSE)->count())
+                ->description('Post Publish'),
+            Stat::make('Student', Student::all()->count()),
+            Stat::make('Teacher', Teacher::all()->count()),
+            Stat::make('Subject', Subject::all()->count()),
         ];
     }
 
---
+++ app/Filament/Resources/SubjectResource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubjectResource\Pages;
+use App\Filament\Resources\SubjectResource\RelationManagers;
+use App\Models\Subject;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+// komponen untuk form
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\TextArea;
+use Illuminate\Support\Str;
+use Filament\Forms\Set;
+// komponen untuk tabel
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\Card;
+
+use Filament\Tables\Contracts\HasTable; //untuk number sort
+
+class SubjectResource extends Resource
+{
+    protected static ?string $model = Subject::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    TextInput::make('kode')
+                        ->required(),
+                    TextInput::make('name')
+                        ->label('Name Subject')
+                        ->required()
+                        ->maxLength(255)
+                        ->reactive()
+                        ->afterStateUpdated(function (Set $set, ?string $state) {
+                        $set('slug', \Str::slug($state));
+                    }),  
+                    TextInput::make('slug')->required()
+                ])->columns(2) //menjadikan 2 kolom
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('No.')->state(
+                static function (HasTable $livewire, $rowLoop): string {
+                    return (string) (
+                        $rowLoop->iteration +
+                        ($livewire->getTableRecordsPerPage() * (
+                            $livewire->getTablePage() - 1
+                        ))
+                    );
+                }
+            ),
+                TextColumn::make('kode')->label('Code'),
+                TextColumn::make('name')->label('Name Subject')->sortable(),
+                TextColumn::make('slug')->limit('50'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSubjects::route('/'),
+        ];
+    }
+}
